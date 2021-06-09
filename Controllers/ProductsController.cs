167fc3e4986626c6cd0efbacd05f96cb3e1cb1be
ChrisJobs046 <ChrisJobs046@gmail.com>
2021-06-09 13:06:13 -0400

    Realizando Test 😁

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using Asp.NetCoreMySecondApi.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Asp.NetCoreMySecondApi.Controllers
@@ -10,11 +12,29 @@ namespace Asp.NetCoreMySecondApi.Controllers
     [Route("api/Products")]
     public class ProductsController: ControllerBase
     {
+        private List<Product> _products = new List<Product>{
+
+            new Product{
+
+                ProductId = 1,
+                Name = "Huawei Y9",
+                Description = "Telefono de la marca Huawei ultimo modelo, en estado perfecto",
+                Price = 17000
+            }
+
+
+
+        };
+
+        [HttpGet]
+        public List<Product> GetProducts() => _products;
+        
+
         //<summary>
         //Get: Products/Home
         //</summary>
 
-        [HttpGet]
-        public string Home() => "Hello World";
+        /* [HttpGet]
+        public string Home() => "Hello World"; */
     }
 }
